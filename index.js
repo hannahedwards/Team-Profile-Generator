d172508815e1ecac0974d0774a7561da137dc422
Hannah Edwards <hannahedwards@MacBook-Pro-2.local>
2023-02-25 23:21:25 -0500

    added html formatting to manager.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const inquirer = require('inquirer');
 const EngineerHtml = require('./lib/Engineer');
 const InternHtml = require('./lib/Intern');
-const ManagerHtml= require('./lib/Manager'); 
+const ManagerHtml = require('./lib/Manager');
 const fs = require('fs');
 
 const questions = [ //start of the function. gathering user input for employees. manager q's
@@ -33,88 +33,88 @@ const questions = [ //start of the function. gathering user input for employees.
     },
 ]
 function init() { //function prompted by the last input answer. will either pull up questions for intern or engineer
-    inquirer.prompt(questions).then((answers) => { 
+    inquirer.prompt(questions).then((answers) => {
         let userChoice = answers.member;
         function switchQuestions() {
             if (userChoice === 'Intern') {
                 console.log("Intern")
                 InternQuestions();
                 return;
-    
-        } else if (userChoice === 'Engineer') {
+
+            } else if (userChoice === 'Engineer') {
                 console.log("Engineer")
                 EngineerQuestions();
                 return;
-    }
-    
-    }
+            }
+
+        }
         switchQuestions();
         const ManagerInput = ManagerHtml(answers);
         fs.writeFile('./dist/index.html', (ManagerInput), (err) =>  //adding to html file
-        console.log('Success') //letting you know it worked
+            console.log('Success') //letting you know it worked
         );
     });
 };
 
 function InternQuestions() { //intern q's
-    const Intern = [ 
+    const Intern = [
         {
-                type: 'input',
-                name: 'InternName',
-                message: 'Enter the interns name.',
-            },
-            {
-                type: 'number',
-                name: 'InternId',
-                message: 'Whats the interns employee ID?',
-            },
-            {
-                type: 'input',
-                name: 'school',
-                message: 'What school is the intern attending?',
-            },
-            {
-                type: 'input',
-                name: 'InternEmail',
-                message: 'Whats the interns email?',
-            },
+            type: 'input',
+            name: 'InternName',
+            message: 'Enter the interns name.',
+        },
+        {
+            type: 'number',
+            name: 'InternId',
+            message: 'Whats the interns employee ID?',
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'What school is the intern attending?',
+        },
+        {
+            type: 'input',
+            name: 'InternEmail',
+            message: 'Whats the interns email?',
+        },
     ]
-    inquirer.prompt(Intern).then((answers) => { 
+    inquirer.prompt(Intern).then((answers) => {
         const InternInput = InternHtml(answers);
         fs.appendFile('./dist/index.html', (InternInput), (err) =>  //adding to html
-        console.log('Success') //letting you know it worked
+            console.log('Success') //letting you know it worked
         );
     });
-    };
+};
 
 function EngineerQuestions() { // engineer q's
-const Engineer = [{
-                type: 'input',
-                name: 'EngineerName',
-                message: 'Enter engineers name',
-            },
-            {
-                type: 'number',
-                name: 'EngineerId',
-                message: 'Whats engineers enemployee ID',
-            },
-            {
-                type: 'input',
-                name: 'EngineerEmail',
-                message: 'Enter engineers email address',
-            },
-            {
-                type: 'input',
-                name: 'githubUrl',
-                message: 'Enter the engineers github URL.',
-            },
-]
-inquirer.prompt(Engineer).then((answers) => { 
-    const EngineerInput = EngineerHtml(answers); 
-    fs.appendFile('./dist/index.html', (EngineerInput), (err) => //addeding to html file
-    console.log('Success')//letting you know it worked
-    );
-});
+    const Engineer = [{
+        type: 'input',
+        name: 'EngineerName',
+        message: 'Enter engineers name',
+    },
+    {
+        type: 'number',
+        name: 'EngineerId',
+        message: 'Whats engineers enemployee ID',
+    },
+    {
+        type: 'input',
+        name: 'EngineerEmail',
+        message: 'Enter engineers email address',
+    },
+    {
+        type: 'input',
+        name: 'githubUrl',
+        message: 'Enter the engineers github URL.',
+    },
+    ]
+    inquirer.prompt(Engineer).then((answers) => {
+        const EngineerInput = EngineerHtml(answers);
+        fs.appendFile('./dist/index.html', (EngineerInput), (err) => //addeding to html file
+            console.log('Success')//letting you know it worked
+        );
+    });
 };
 
 init(); // calls the funtion
